6d9470e5d0222e3e2ee83bc544fd512a9f6e29f9
smiah4433 <smiah4433@gmail.com>
2021-12-08 14:36:15 -0600

    foundation completed

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ class Tamagotchi {
     }
   }
   bordnessUp() {
-    if (this.bordness <= 10){
+    if (this.bordness <= 8){
       this.bordness +=2
       this.bordnessText.innerText = this.bordness
       setTimeout(() => this.bordnessUp(), 2000)
@@ -34,15 +34,37 @@ class Tamagotchi {
       setTimeout(() => this.hungerUp(), 2000)
     }
   }
+  sleepUp() {
+    if (this.sleepiness <=10) {
+      this.bordness-1
+      this.bordnessText.innerText = this.bordness
+    }
+  }
 
   feed() {
     this.hunger--
+    if (this.hunger < 10)
     this.hungerText.innerText = this.hunger
     this.bordness-=2
     this.bordnessText.innerText = this.bordness
-    this.sleepiness+=2
+    if (this.sleepiness < 10) {
+      this.sleepiness+=2
+      this.sleepinessText.innerText = this.sleepiness
+    }
+  }
+
+  sleep() {
+    this.hunger = 0
+    this.hungerText.innerText = this.hunger
+    this.bordness = 0
+    // if (this.bordness > 9) {
+    //   alert("Mr G has died!")
+    // }
+    this.bordnessText.innerText = this.bordness
+    this.sleepiness = 0
     this.sleepinessText.innerText = this.sleepiness
   }
+
 }
 
 let tamagotchi = new Tamagotchi()
@@ -51,5 +73,23 @@ function play() {
   tamagotchi.ageUp()
   tamagotchi.hungerUp()
   tamagotchi.bordnessUp()
+  tamagotchi.feed()
   tamagotchi.sleep()
+
 }
+
+
+
+// function Sleep() {
+//   // this.SleepText.inne = this.Sleep
+// //   // tamagotchi.hungerUp()
+//   this.hunger = 0
+//
+//   this.hungerText = document.getElementById('hunger')
+// //   // tamagotchi.bordnessUp()
+//   this.bordness = 0
+//   // this.bordnessText = document.getElementById('bordness')
+//   this.sleepiness = 0
+// //   console.log(reset);
+//   this.sleepinessText = document.getElementById('sleepiness')
+// }
