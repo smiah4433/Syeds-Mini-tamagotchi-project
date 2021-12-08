e9a840d25b3140a68154310649b7e4ff5f17cdbe
smiah4433 <smiah4433@gmail.com>
2021-12-08 08:59:22 -0600

    Two Buttons working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
-alert('We are pushing')
+class Tamagotchi {
+  constructor() {
+    this.age = 0
+    this.ageText = document.getElementById('age')
+    this.hunger = 0
+    this.hungerText = document.getElementById('hunger')
+  }
 
-console.log("We are pushing as well");
+  ageUp() {
+    if (this.age <= 30) {
+      this.age += 2
+      this.ageText.innerText = this.age
+      // console.log(age);
+      setTimeout(() => this.ageUp(), 3000)
+    }
+  }
+
+  hungerUp() {
+    if (this.hunger <= 10) {
+      this.hunger++
+      this.hungerText.innerText = this.hunger
+      // console.log(age);
+      setTimeout(() => this.hungerUp(), 2000)
+    }
+  }
+
+  feed() {
+    this.hunger--
+    this.hungerText.innerText = this.hunger
+  }
+}
+
+let tamagotchi = new Tamagotchi()
+
+function play() {
+  tamagotchi.ageUp()
+  tamagotchi.hungerUp()
+}
